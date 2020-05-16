deb781c3a366a448023f5350c262f794230eb50d
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-17 02:07:45 +0500

    changed

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -15,12 +15,12 @@ class TicketManagerTest {
     private TicketOffer thirdTicket = new TicketOffer(3, 11000, "LEG", "DME", 100);
     private TicketOffer fourthTicket = new TicketOffer(4, 10500, "DME", "LEG", 125);
     private TicketOffer fifthTicket = new TicketOffer(5, 10200, "KUF", "LEG", 220);
-    private TicketOffer sixthTicket = new TicketOffer(6, 11200, "DME", "LEG", 110);
+    private TicketOffer sixthTicket = new TicketOffer(6, 10200, "KUF", "LEG", 110);
     private TicketOffer seventhTicket = new TicketOffer(7, 13000, "DME", "KUF", 210);
     private TicketOffer eighthTicket = new TicketOffer(8, 8000, "LEG", "DME", 140);
     private TicketOffer ninthTicket = new TicketOffer(9, 10900, "LEG", "KUF", 215);
     private TicketOffer tenthTicket = new TicketOffer(10, 10700, "KUF", "LEG", 230);
-    private TicketOffer eleventhTicket = new TicketOffer(11, 12000, "DME", "LEG", 100);
+    private TicketOffer eleventhTicket = new TicketOffer(11, 10700, "KUF", "LEG", 100);
 
     @BeforeEach
     void init() {
@@ -61,7 +61,7 @@ class TicketManagerTest {
 
     @Test
     void shouldfindAllSortAfteRemoveNoCorrect() {
-        manager.remById(100);
+        manager.remById(1000);
         TicketOffer[] actual = manager.findAllSort("LEG", "DME");
         TicketOffer[] expected = new TicketOffer[]{eighthTicket, fistTicket, thirdTicket};
         assertArrayEquals(expected, actual);
